3f33722498889545e3bfffc660a75de41c35356a
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2022-09-24 16:00:45 -0700

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,2 +1,2 @@
-document.getElementById("splash-image").style.backgroundImage = "url('https://github.com/Mr-funkinguy/prodigy-loader-api/blob/main/Untitled_Artwork%20232.png?raw=true')";
+document.getElementById("splash-image").style.backgroundImage = "url('https://github.com/Mr-funkinguy/prodigy-loader-api/blob/main/Untitled_Artwork%20234.png?raw=true')";
 //# sourceMappingURL=contentScript.js.map
